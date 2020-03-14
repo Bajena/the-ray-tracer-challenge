3f3506a433a14c63d4b67a32b2a47ec50a4f86f4
Jan Bajena <bajena3@gmail.com>
2020-03-14 12:50:52 +0100

    Set up mix project

---
+++ lib/ray_tracer.ex
@@ -0,0 +1,18 @@
+defmodule RayTracer do
+  @moduledoc """
+  Documentation for RayTracer.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RayTracer.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ray_tracer_test.exs
@@ -0,0 +1,8 @@
+defmodule RayTracerTest do
+  use ExUnit.Case
+  doctest RayTracer
+
+  test "greets the world" do
+    assert RayTracer.hello() == :world
+  end
+end
