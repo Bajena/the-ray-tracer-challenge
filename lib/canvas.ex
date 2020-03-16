f37ca53ea690986dab4f2fb9340de43ad06b6c5e
Jan Bajena <bajena3@gmail.com>
2020-03-16 09:09:57 +0100

    Fix CC issues

--- lib/canvas.ex
+++ lib/canvas.ex
@@ -20,7 +20,7 @@ defmodule RayTracer.Canvas do
   end
 
   @spec pixel_at(t, integer, integer) :: RTuple.t
-  def pixel_at(canvas, x, y), do: canvas.pixels[{x,y}]
+  def pixel_at(canvas, x, y), do: canvas.pixels[{x, y}]
 
   @spec write_pixel(t, integer, integer, RTuple.t) :: t
   def write_pixel(canvas = %__MODULE__{pixels: p}, x, y, color) do
@@ -28,7 +28,7 @@ defmodule RayTracer.Canvas do
       raise ArgumentError, message: "Position out of bounds: #{x}, #{y}"
     end
 
-    new_pixels = p |> Map.put({x,y}, color)
+    new_pixels = p |> Map.put({x, y}, color)
     %__MODULE__{canvas | pixels: new_pixels}
   end
 
@@ -38,7 +38,7 @@ defmodule RayTracer.Canvas do
     new_x = if x == w - 1, do: 0, else: x + 1
     new_y = if x == w - 1, do: y + 1, else: y
     pixels |>
-    Map.put({x,y}, Color.new(0, 0, 0))
+    Map.put({x, y}, Color.new(0, 0, 0))
     |> build_pixels(new_x, new_y, w, h)
   end
 end
--- lib/r_tuple/helpers.ex
+++ lib/r_tuple/helpers.ex
@@ -6,12 +6,13 @@ defmodule RayTracer.RTuple.Helpers do
 
   @spec zip_map(RTuple.t, RTuple.t, function()) :: list(number)
   def zip_map(%{values: v1}, %{values: v2}, fun) do
-    Enum.zip(Tuple.to_list(v1), Tuple.to_list(v2))
+    Tuple.to_list(v1)
+    |> Enum.zip(Tuple.to_list(v2))
     |> Enum.map(fn {x, y} -> fun.(x, y) end)
   end
 
   @spec map(RTuple.t, function()) :: list(number)
   def map(%{values: v}, fun) do
-    Tuple.to_list(v) |> Enum.map(fun)
+    v |> Tuple.to_list |> Enum.map(fun)
   end
 end
