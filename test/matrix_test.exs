59ce67a0673d888e016503667f61a48af0e4fcbb
Jan Bajena <bajena3@gmail.com>
2020-03-21 16:43:51 +0100

    Add multiplication test

--- test/matrix_test.exs
+++ test/matrix_test.exs
@@ -1,7 +1,7 @@
 defmodule MatrixTest do
   alias RayTracer.Matrix
   use ExUnit.Case
-  doctest RayTracer.Matrix
+  # doctest RayTracer.Matrix
 
   test "constructing and inspecting a 4x4 matrix" do
     m =
@@ -20,5 +20,68 @@ defmodule MatrixTest do
     assert m |> Matrix.elem(2,2) == 11
     assert m |> Matrix.elem(3,2) == 15.5
     assert m |> Matrix.elem(3,5) == nil
+  end
+
+  test "comparing matrices" do
+    m1 =
+      """
+      | 1    | 2     | 3     | 4     |
+      | 5.5  | 6.5   | 7.5   | 8.5   |
+      | 9    | 10    | 11    | 12    |
+      | 13.5 | 14.5  | 15.5  | 16.5  |
+      """
+      |> Matrix.from_string
+
+    m2 =
+      """
+      | 1    | 2     | 3     | 4     |
+      | 5.5  | 6.5   | 7.5   | 8.5   |
+      | 9    | 10    | 11    | 12    |
+      | 13.5 | 14.5  | 15.5  | 16.5  |
+      """
+      |> Matrix.from_string
+
+    m3 =
+      """
+      | 2    | 2     | 3     | 4     |
+      | 5.5  | 6.5   | 7.5   | 8.5   |
+      | 9    | 10    | 11    | 12    |
+      | 13.5 | 14.5  | 15.5  | 16.5  |
+      """
+      |> Matrix.from_string
+
+    assert m1 == m2
+    assert m1 != m3
+  end
+
+  test "multiplying matrices" do
+    m1 =
+      """
+      | 1 | 2 | 3 | 4 |
+      | 5 | 6 | 7 | 8 |
+      | 9 | 8 | 7 | 6 |
+      | 5 | 4 | 3 | 2 |
+      """
+      |> Matrix.from_string
+
+    m2 =
+      """
+      | -2 | 1 | 2 |  3 |
+      |  3 | 2 | 1 | -1 |
+      |  4 | 3 | 6 |  5 |
+      |  1 | 2 | 7 |  8 |
+      """
+      |> Matrix.from_string
+
+    m3 =
+      """
+      | 20 | 22 |  50 |  48 |
+      | 44 | 54 | 114 | 108 |
+      | 40 | 58 | 110 | 102 |
+      | 16 | 26 |  46 |  42 |
+      """
+      |> Matrix.from_string
+
+    assert Matrix.mult(m1, m2) == m3
   end
 end
