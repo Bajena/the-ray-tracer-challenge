99aaec4a14089f306245cff384012d84078573aa
Jan Bajena <bajena3@gmail.com>
2020-03-28 15:14:38 +0100

    Translation specs

--- test/transformations_test.exs
+++ test/transformations_test.exs
@@ -12,4 +12,19 @@ defmodule TransformationsTest do
 
     assert t |> Matrix.mult(p) == RTuple.point(2, 1, 7)
   end
+
+  test "Multiplying by the inverse of a translation matrix" do
+    inv = translation(5, -3, 2) |> Matrix.inverse
+    p = RTuple.point(-3, 4, 5)
+
+    assert inv |> Matrix.mult(p) == RTuple.point(-8, 7, 3)
+  end
+
+
+  test "Translation does not affect vectors" do
+    t = translation(5, -3, 2)
+    v = RTuple.vector(-3, 4, 5)
+
+    assert t |> Matrix.mult(v) == v
+  end
 end
