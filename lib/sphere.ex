96fca48682a225674356185f534d7e036ee83b1f
Jan Bajena <bajena3@gmail.com>
2020-03-30 21:12:20 +0200

    Chapter 5: Finalize ray-sphere intersections

--- lib/sphere.ex
+++ lib/sphere.ex
@@ -4,19 +4,26 @@ defmodule RayTracer.Sphere do
   """
 
   alias RayTracer.RTuple
+  alias RayTracer.Matrix
 
   @type t :: %__MODULE__{
     center: RTuple.point,
-    r: number
+    r: number,
+    transform: Matrix.matrix
   }
 
-  defstruct [:center, :r]
+  defstruct [:center, :r, :transform]
 
   @doc """
-  Builds a sphere with given `center` and radius `r`
+  Builds a sphere with given `center`, radius `r` and transformation matrix
   """
   @spec new(RTuple.point, number) :: t
-  def new(center \\ RTuple.point(0, 0, 0), radius \\ 1) do
-    %__MODULE__{center: center, r: radius}
+  def new(center \\ RTuple.point(0, 0, 0), radius \\ 1, transform \\ Matrix.ident(4)) do
+    %__MODULE__{center: center, r: radius, transform: transform}
+  end
+
+  @spec new(t, Matrix.matrix) :: t
+  def set_transform(s, t) do
+    %__MODULE__{s | transform: t}
   end
 end
--- test/sphere_test.exs
+++ test/sphere_test.exs
@@ -1,10 +1,12 @@
 defmodule SphereTest do
   alias RayTracer.RTuple
+  alias RayTracer.Matrix
   alias RayTracer.Ray
   alias RayTracer.Sphere
   alias RayTracer.Intersection
   import RTuple, only: [point: 3, vector: 3]
   import Intersection, only: [intersect: 2, hit: 1]
+  import RayTracer.Transformations
 
   use ExUnit.Case
   doctest RayTracer.Sphere
@@ -57,6 +59,16 @@ defmodule SphereTest do
     assert Enum.at(xs, 1).t == -4
   end
 
+  test "Intersect sets the object on the intersection" do
+    r = Ray.new(point(0, 0, -5), vector(0, 0, 1))
+    s = Sphere.new()
+
+    xs = intersect(s, r)
+    assert length(xs) == 2
+    assert Enum.at(xs, 0).object == s
+    assert Enum.at(xs, 1).object == s
+  end
+
   test "The hit, when all intersections have positive t" do
     s = Sphere.new()
     i1 = Intersection.new(1, s)
@@ -93,5 +105,35 @@ defmodule SphereTest do
     xs = [i1, i2, i3, i4]
 
     assert hit(xs) == i4
+  end
+
+  test "A sphere's default transformation" do
+    s = Sphere.new()
+
+    assert s.transform == Matrix.ident(4)
+  end
+
+  test "Changing a sphere's transformation" do
+    s = Sphere.new()
+    t = translation(2, 3, 4)
+    assert Sphere.set_transform(s, t).transform == t
+  end
+
+  test "Intersecting a scaled sphere with a ray" do
+    r = Ray.new(point(0, 0, -5), vector(0, 0, 1))
+    s = Sphere.new() |> Sphere.set_transform(scaling(2, 2, 2))
+    xs = intersect(s, r)
+
+    assert length(xs) == 2
+    assert Enum.at(xs, 0).t == 3
+    assert Enum.at(xs, 1).t == 7
+  end
+
+  test "Intersecting a translated sphere with a ray" do
+    r = Ray.new(point(0, 0, -5), vector(0, 0, 1))
+    s = Sphere.new() |> Sphere.set_transform(translation(5, 0, 0))
+    xs = intersect(s, r)
+
+    assert length(xs) == 0
   end
 end
